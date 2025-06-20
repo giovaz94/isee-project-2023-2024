34d87e5184110ffea513fbb3c8c561b5c74e0a13
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-06-20 11:49:28 +0200

    feat: add max round duration and food pieces configurations

--- src/main/kotlin/io/github/evasim/controller/SimulatorController.kt
+++ src/main/kotlin/io/github/evasim/controller/SimulatorController.kt
@@ -10,6 +10,7 @@ import io.github.evasim.utils.logger
 import it.unibo.jakta.agents.bdi.dsl.mas
 import it.unibo.jakta.agents.bdi.executionstrategies.ExecutionStrategy
 import kotlin.concurrent.thread
+import kotlin.time.Duration
 
 /**
  * Represents the simulation controller interface responsible for managing user inputs, events,
@@ -18,9 +19,10 @@ import kotlin.concurrent.thread
 interface Controller : EventPublisher {
 
     /**
-     * Starts the simulation with the given [configuration].
+     * Starts the simulation with the given [configuration],
+     * possibly giving a [roundTimeout] to limit the duration of each round.
      */
-    fun start(configuration: World.Companion.Configuration)
+    fun start(configuration: World.Companion.Configuration, roundTimeout: Duration? = null)
 
     /**
      * Stops the current simulation.
@@ -34,14 +36,11 @@ object SimulatorController : Controller, EventBusPublisher() {
     private var activeSimulation: Round? = null
 
     @Synchronized
-    override fun start(configuration: World.Companion.Configuration) {
+    override fun start(configuration: World.Companion.Configuration, roundTimeout: Duration?) {
         check(activeSimulation == null) { "A simulation is already running. Please, stop it first." }
         World.fromConfiguration(configuration).also { world ->
-            // TODO: make it configurable the round and simulation end criteria
-            // val initialRound = Round.byNoFood(world)
-            // TODO: just for testing until we have a proper contention in place
             val initialRound = Round.byCriteria(world) {
-                it.world.foods.count() == 0
+                it.elapsedTime >= (roundTimeout ?: Duration.INFINITE) || it.world.foods.toSet().isEmpty()
             }
             thread { simulationLoop(initialRound) }
             activeSimulation = initialRound
@@ -53,7 +52,7 @@ object SimulatorController : Controller, EventBusPublisher() {
         subscribers.forEach { round.world.register(it) }
         mas(round).start()
         logger.info { "Round ${round.number} ended." }
-        if (!shouldStop(round)) {
+        if (!shouldStop(round) && !round.world.blobs.toSet().isEmpty()) {
             val nextRound = round.next()
             activeSimulation = nextRound
             simulationLoop(nextRound)
--- src/main/kotlin/io/github/evasim/view/controllers/ControlsPaneController.kt
+++ src/main/kotlin/io/github/evasim/view/controllers/ControlsPaneController.kt
@@ -14,6 +14,8 @@ import javafx.scene.control.TextField
 import javafx.scene.layout.VBox
 import java.net.URL
 import java.util.*
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 @Suppress("detekt:VarCouldBeVal")
 internal class ControlsPaneController : Initializable {
@@ -34,18 +36,31 @@ internal class ControlsPaneController : Initializable {
 
     @FXML private lateinit var hawkCountField: TextField
 
+    @FXML private lateinit var foodPiecesField: TextField
+
+    @FXML private lateinit var roundMaxDuration: TextField
+
     override fun initialize(location: URL?, resources: ResourceBundle?) {
         ToggleController(toggleButton, controlsPanel)
+        doveCountField.textProperty().addListener { recomputeFoodField() }
+        hawkCountField.textProperty().addListener { recomputeFoodField() }
         startButton.setOnAction { onStart() }
         stopButton.setOnAction { onStop() }
         showBlobInfoCheckBox.setOnAction { simulationPaneController?.toggleShowBlobNames() }
     }
 
+    private fun recomputeFoodField() {
+        val doves = doveCountField.text.toIntOrNull() ?: 0
+        val hawks = hawkCountField.text.toIntOrNull() ?: 0
+        foodPiecesField.text = (doves + hawks).toString()
+    }
+
     private fun onStart() {
-        // TODO: validate input
         val hawkyBlobs = hawkCountField.text.toInt()
         val doveBlobs = doveCountField.text.toInt()
+        val maxRoundDuration = roundMaxDuration.text.toIntOrNull()?.toDuration(DurationUnit.SECONDS)
         val radius = BASE_RADIUS + (hawkyBlobs + doveBlobs) * SCALE_FACTOR
+        // TODO: enlarge the world shape based on the number of blobs
         val config = World.Companion.Configuration(
             shape = Circle(radius),
             spawnZones = setOf(
@@ -53,8 +68,9 @@ internal class ControlsPaneController : Initializable {
             ),
             hawkyBlobs = hawkyBlobs,
             doveBlobs = doveBlobs,
+            foodsAmount = foodPiecesField.text.toInt(),
         )
-        SimulatorController.start(config)
+        SimulatorController.start(config, maxRoundDuration)
         simulationPaneController?.newSimulationState(SimulationState.RUNNING)
     }
 
